7913789561c451f66762ed820985f6d42811d2a8
dev-ayomide <ayomidepaul784@gmail.com>
2025-09-04 09:31:27 +0100

    refactor: remove fallback

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://campor-aa1452bb8116.herokuapp.com/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const RUN_EMAIL_REGEX = /^[^\s@]+@run\.edu\.ng$/i;
 
 // API Endpoints
